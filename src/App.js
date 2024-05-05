94eef1c786a75a67bed9f46b57159d51a22bdd7c
MS892 <mschmitz92@gmail.com>
2024-05-05 02:34:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import fleekLogo from './fleek-logo.png';
-import reactLogo from './react-logo.svg';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const importAll = (r) => r.keys().map(r);
+    const imageFiles = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+    setImages(imageFiles);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
-        <span className="Big-plus">+</span>
-        <img src={reactLogo} className="React-logo" alt="react-logo" />
-        <p>
-          React App deployed on IPFS through Fleek.
-        </p>
-        <a
-          className="App-link"
-          href="https://docs.fleek.co"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More About Fleek!
-        </a>
+        <h1>CryptoCanvas Gallery</h1>
       </header>
+      <div className="Image-gallery">
+        {images.map((image, index) => (
+          <img key={index} src={image.default} alt={`Image ${index}`} className="Gallery-image" />
+        ))}
+      </div>
     </div>
   );
 }
